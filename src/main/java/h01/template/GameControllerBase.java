1bc3674e59bd447ec13d2091bd4351863bdde46e
Rdeisenroth <rdeisenroth@gmail.com>
2023-10-18 22:05:55 +0200

    fix start coin multiplier not being used

--- src/main/java/h01/template/GameControllerBase.java
+++ src/main/java/h01/template/GameControllerBase.java
@@ -168,14 +168,14 @@ public abstract class GameControllerBase {
                 World.getWidth() - 1,
                 0,
                 Direction.UP,
-                5 * World.getWidth() * World.getHeight()
+                GameConstants.CONTAMINANT_ONE_START_COINS_MULTIPLIER * World.getWidth() * World.getHeight()
             )
         );
         this.robots.add(contaminant2 = new Contaminant2(
                 World.getWidth() - 1,
                 World.getHeight() - 1,
                 Direction.UP,
-                2 * World.getWidth() * World.getHeight()
+                GameConstants.CONTAMINANT_TWO_START_COINS_MULTIPLIER * World.getWidth() * World.getHeight()
             )
         );
     }
